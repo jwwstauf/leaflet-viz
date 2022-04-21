e32378279a8030b57a940ccfbaeb105b7570664c
John Stauffer <jwwstauf@gmail.com>
2022-04-20 19:05:07 -0700

    Last one

--- Step-1/static/js/logic.js
+++ Step-1/static/js/logic.js
@@ -1,13 +1,3 @@
-var basemap = L.tileLayer(
-  "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
-  {
-    tileSize: 512,
-    maxZoom: 18,
-    zoomOffset: -1,
-    id: "mapbox/light-v10",
-  }
-);
-
 var map = L.map("map", {
   center: [
     0, 0
@@ -15,58 +5,42 @@ var map = L.map("map", {
   zoom:3
 });
 
-
-basemap.addTo(map);
-
+var basemap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
 
 d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(data) {
 
 
-  function styleInfo(feature) {
+  function stylin(feature) {
     return {
       opacity: .5,
       fillOpacity: .5,
       fillColor: markerColor(feature.geometry.coordinates[2]),
       color: "grey",
       radius: getSize(feature.properties.mag),
-      stroke: true,
       weight: 0.5
     };
   }
 
   function getSize(magnitude) {
-    if (magnitude === 0) {
-      return 1;
-    }
+    if (magnitude === 0) return 1;
     return magnitude * 5;
   }
 
   function markerColor(depth){
-    
-    
-    if (depth > 20) {
-        return "red";
-    }
-    else if (depth > 10) {
-        return "orange";
-    }
-    else if (depth > 2) {
-        return "yellow";
-    }
-    else {
-        return "green";
-    }
-
+       
+    if (depth > 20) return "red";
+    else if (depth > 10) return "orange";
+    else if (depth > 2) return "yellow";
+    else return "green";
 }
-
-  
-
 
   L.geoJson(data, {
     pointToLayer: function(feature, latlng) {
       return L.circleMarker(latlng);
     },
-    style: styleInfo,
+    style: stylin,
     onEachFeature: function(feature, layer) {
       layer.bindPopup(
         "Magnitude: "
